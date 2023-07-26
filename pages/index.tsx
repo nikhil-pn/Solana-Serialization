05e82eccaa1553129f987295eeef229661f48f94
nikhilpn725 <nikhilpn725@gmail.com>
2023-07-26 21:07:51 +0530

    deserialize func added, movies review fetched from blockchain

--- pages/index.tsx
+++ pages/index.tsx
@@ -16,13 +16,7 @@ const Home: NextPage = () => {
       <AppBar />
       <Center>
         <Box>
-          {/* <Heading ml={4} mt={8}>
-            Add a review
-          </Heading> */}
           <Form />
-          {/* <Heading as="h1" size="l" color="white" ml={4} mt={8}>
-            Existing Reviews
-          </Heading> */}
           <MovieList />
         </Box>
       </Center>
